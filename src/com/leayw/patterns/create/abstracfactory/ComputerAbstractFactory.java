69926beaa452141b33321da711d947ef040ab4ab
liyawei <liyawei@foryou56.com>
2019-01-14 11:51:46 +0800

    abstract factory

---
+++ src/com/leayw/patterns/create/abstracfactory/ComputerAbstractFactory.java
@@ -0,0 +1,7 @@
+package com.leayw.patterns.create.abstracfactory;
+
+import com.leayw.patterns.create.factory.Computer;
+
+public interface ComputerAbstractFactory {
+    public Computer createComputer();
+}
--- src/com/leayw/patterns/create/factory/ComputorFactory.java
+++ src/com/leayw/patterns/create/factory/ComputorFactory.java
@@ -1,5 +1,10 @@
 package com.leayw.patterns.create.factory;
 
+/**
+ * Factory Design Pattern Examples in JDK
+ * java.util.Calendar, ResourceBundle and NumberFormat getInstance() methods uses Factory pattern.
+ * valueOf() method in wrapper classes like Boolean, Integer etc.
+ */
 public class ComputorFactory {
     public static Computer getComputor(String type, String ram, String hdd, String cpu) {
         switch (type) {
