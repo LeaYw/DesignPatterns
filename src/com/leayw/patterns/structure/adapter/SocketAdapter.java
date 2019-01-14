4892d433779d9db1c2d7b47dcbab6d79007d3f18
liyawei <liyawei@foryou56.com>
2019-01-14 14:29:36 +0800

    add adapter jdk demo

--- src/com/leayw/patterns/structure/adapter/SocketAdapter.java
+++ src/com/leayw/patterns/structure/adapter/SocketAdapter.java
@@ -1,5 +1,13 @@
 package com.leayw.patterns.structure.adapter;
 
+
+/**
+ *  Adapter Design Pattern Example in JDK
+ *
+ * java.util.Arrays#asList()
+ * java.io.InputStreamReader(InputStream) (returns a Reader)
+ * java.io.OutputStreamWriter(OutputStream) (returns a Writer)
+ */
 public interface SocketAdapter {
 
     Volt get120Volt();
